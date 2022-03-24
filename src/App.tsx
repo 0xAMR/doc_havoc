e9f85d004d3f0eae5c49320752676dcf5eeb4551
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2022-03-24 22:22:03 +0000

    ✨ navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+// Components
+import Navbar from './components/Navbar';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>DocFree</h1>
+      <Navbar />
     </div>
   );
 }
-
-export default App;
