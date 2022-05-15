83927cbf853f236937c2ab81aa9ebbad7875a094
andreiracasan <69733376+AndreiRacasan@users.noreply.github.com>
2022-05-15 20:50:56 +0100

    ✨ create overview

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 // Components
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Overview from './components/Overview';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
       <Hero
         title="Doc Havoc is Here"
         subtitle="Lorem Ipsum Dolor Sit Amet"
         cta="Learn More"
       />
+      <Overview />
     </div>
   );
 }
---
+++ src/components/Overview/index.tsx
@@ -0,0 +1,36 @@
+// Styling
+import './overview.scss';
+
+export default function Overview() {
+  return (
+    <div className="overview">
+      <div className="overview__container">
+        <div className="overview__card">
+          <h3>Title I</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam
+            omnis nesciunt velit alias accusantium necessitatibus libero
+            recusandae non! Non perferendis adipisci labore aperiam nam.
+          </p>
+        </div>
+        <div className="overview__card">
+          <h3>Title II</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            saepe ratione perferendis nisi laborum sint dolores veniam in vitae
+            ipsum.
+          </p>
+        </div>
+        <div className="overview__card">
+          <h3>Title III</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            deleniti quasi inventore atque ut. Quo nisi fugiat esse. Sit modi
+            recusandae consectetur sequi ut nemo dolor ipsam. Dolor, suscipit
+            cumque.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
